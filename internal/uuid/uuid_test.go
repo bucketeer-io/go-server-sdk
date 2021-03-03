4b2f866712cc540a56c1c90c4fe912f77e84ac7e
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2021-03-03 17:37:27 +0900

    chore: parallelize unit test

--- internal/uuid/uuid_test.go
+++ internal/uuid/uuid_test.go
@@ -11,6 +11,7 @@ import (
 var v4Regex = regexp.MustCompile("^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[89abAB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$")
 
 func TestNewV4(t *testing.T) {
+	t.Parallel()
 	for cnt := 0; cnt < 32; cnt++ {
 		id, err := NewV4()
 		require.NoError(t, err)
