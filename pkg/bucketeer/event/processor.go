e6d89fe3fd3db2980e16eed9f3fce198f9900956
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2021-03-08 17:18:47 +0900

    fix(event): processor comment

--- pkg/bucketeer/event/processor.go
+++ pkg/bucketeer/event/processor.go
@@ -11,31 +11,31 @@ import (
 
 // Processor defines the interface for processing events.
 //
-// When one of the Push method called, Processor pushes an event to the event queue.
+// When one of the Push method called, Processor pushes an event to the in-memory event queue.
 //
 // In the background, Processor pulls the events from the event queue,
 // and sends them to the Bucketeer server using the Bucketeer API Client
 // every time the specified time elapses or the specified capacity is exceeded.
 type Processor interface {
-	// PushEvaluationEvent pushes the evaluation event to the event queue.
+	// PushEvaluationEvent pushes the evaluation event to the queue.
 	PushEvaluationEvent(ctx context.Context, user *protouser.User, evaluation *protofeature.Evaluation)
 
-	// PushDefaultEvaluationEvent pushes the default evaluation event to the event queue.
+	// PushDefaultEvaluationEvent pushes the default evaluation event to the queue.
 	PushDefaultEvaluationEvent(ctx context.Context, user *protouser.User, featureID string)
 
-	// PushEvaluationEvent pushes the goal event to the event queue.
+	// PushEvaluationEvent pushes the goal event to the queue.
 	PushGoalEvent(ctx context.Context, user *protouser.User, goalID string, value float64)
 
-	// PushGetEvaluationLatencyMetricsEvent pushes the get evaluation latency metrics event to the event queue.
+	// PushGetEvaluationLatencyMetricsEvent pushes the get evaluation latency metrics event to the queue.
 	PushGetEvaluationLatencyMetricsEvent(ctx context.Context, duration time.Duration, tag, state string)
 
-	// PushGetEvaluationSizeMetricsEvent pushes the get evaluation size metrics event to the event queue.
+	// PushGetEvaluationSizeMetricsEvent pushes the get evaluation size metrics event to the queue.
 	PushGetEvaluationSizeMetricsEvent(ctx context.Context, sizeByte int, tag, state string)
 
-	// PushTimeoutErrorCountMetricsEvent pushes the timeout error count metrics event to the event queue.
+	// PushTimeoutErrorCountMetricsEvent pushes the timeout error count metrics event to the queue.
 	PushTimeoutErrorCountMetricsEvent(ctx context.Context, tag string)
 
-	// PushInternalErrorCountMetricsEvent pushes the internal error count metrics event to the event queue.
+	// PushInternalErrorCountMetricsEvent pushes the internal error count metrics event to the queue.
 	PushInternalErrorCountMetricsEvent(ctx context.Context, tag string)
 
 	// Close tears down all Processor activities, after ensuring that all events have been delivered.
