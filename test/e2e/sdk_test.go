bb098629bb969b6cf827cc6d066d3c4a2a96826a
Seiya Muramatsu <seiya.muramatsu1@gmail.com>
2021-03-22 19:22:04 +0900

    test(e2e): add sdk e2e test

--- test/e2e/sdk_test.go
+++ test/e2e/sdk_test.go
@@ -1,3 +1,77 @@
 package e2e
 
-// TODO: implement below
+import (
+	"context"
+	"fmt"
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/ca-dp/bucketeer-go-server-sdk/pkg/bucketeer"
+)
+
+func TestSDK(t *testing.T) {
+	t.Parallel()
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+	sdk := newSDK(t, ctx)
+	defer func() {
+		// Close
+		err := sdk.Close(ctx)
+		assert.NoError(t, err)
+	}()
+	var wg sync.WaitGroup
+	testVariationFunc := func(ctx context.Context, userID, featureID, expectedVariation string) {
+		defer wg.Done()
+		user := newUser(t, userID)
+		v := sdk.StringVariation(ctx, user, featureID, "default")
+		assert.Equal(t, expectedVariation, v)
+	}
+
+	// Get Variation by Default Strategy (featureIDVariation1)
+	for i := 0; i < 3; i++ {
+		wg.Add(1)
+		userID := fmt.Sprintf("user-%d", i)
+		go testVariationFunc(ctx, userID, featureID, featureIDVariation1)
+	}
+
+	// Get Variation by Targeting Users (featureIDVariation2)
+	wg.Add(1)
+	go testVariationFunc(ctx, userID, featureID, featureIDVariation2)
+
+	// Track
+	wg.Add(1)
+	go func(ctx context.Context, userID, goalID string) {
+		defer wg.Done()
+		user := newUser(t, userID)
+		sdk.Track(ctx, user, goalID)
+	}(ctx, userID, goalID)
+
+	wg.Wait()
+}
+
+func newSDK(t *testing.T, ctx context.Context) bucketeer.SDK {
+	t.Helper()
+	sdk, err := bucketeer.NewSDK(
+		ctx,
+		bucketeer.WithTag(tag),
+		bucketeer.WithAPIKey(*apiKey),
+		bucketeer.WithHost(*host),
+		bucketeer.WithPort(*port),
+		bucketeer.WithEventQueueCapacity(100),
+		bucketeer.WithNumEventFlushWorkers(3),
+		bucketeer.WithEventFlushSize(5),
+		bucketeer.WithEnableDebugLog(true),
+	)
+	require.NoError(t, err)
+	return sdk
+}
+
+func newUser(t *testing.T, id string) *bucketeer.User {
+	t.Helper()
+	user, err := bucketeer.NewUser(id, map[string]string{"attr-key": "attr-value"})
+	require.NoError(t, err)
+	return user
+}
