23c1fb6a69e021bd260e240263cca4b8c171714c
Naoto Ono <onoto1998@gmail.com>
2022-09-15 16:16:22 +0900

    fix: opencensus metrics not reporting success status (#93)

---
+++ pkg/bucketeer/api/status_test.go
@@ -0,0 +1,41 @@
+package api
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetStatusCode(t *testing.T) {
+	t.Parallel()
+	patterns := map[string]struct {
+		input        error
+		expectedCode int
+		expectedBool bool
+	}{
+		"StatusInternalServerError": {
+			input:        NewErrStatus(http.StatusInternalServerError),
+			expectedCode: http.StatusInternalServerError,
+			expectedBool: true,
+		},
+		"status ok": {
+			input:        nil,
+			expectedCode: http.StatusOK,
+			expectedBool: true,
+		},
+		"unknown error": {
+			input:        errors.New("error"),
+			expectedCode: 0,
+			expectedBool: false,
+		},
+	}
+	for msg, p := range patterns {
+		t.Run(msg, func(t *testing.T) {
+			code, ok := GetStatusCode(p.input)
+			assert.Equal(t, p.expectedCode, code)
+			assert.Equal(t, p.expectedBool, ok)
+		})
+	}
+}
