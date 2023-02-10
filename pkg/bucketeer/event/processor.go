ec5516396a79f8fc05f800bd59fcfde5d319c439
Naoto Ono <onoto1998@gmail.com>
2023-02-10 18:07:14 +0900

    fix

--- pkg/bucketeer/event/processor.go
+++ pkg/bucketeer/event/processor.go
@@ -307,8 +307,7 @@ func (p *processor) PushErrorStatusCodeMetricsEvent(ctx context.Context, api mod
 	case http.StatusServiceUnavailable:
 		evt = model.NewServiceUnavailableErrorMetricsEvent(p.tag, api)
 	default:
-		p.loggers.Errorf("bucketeer/event: PushErrorStatusCodeMetricsEvent failed (err: %v, tag: %s",
-			fmt.Errorf("unknown status code: %d", code), p.tag)
+		evt = model.NewUnknownErrorMetricsEvent(p.tag, api)
 		return
 	}
 	encodedESCMetricsEvt, err := json.Marshal(evt)
---
+++ pkg/bucketeer/model/unknown_error_metrics_event.go
@@ -0,0 +1,17 @@
+package model
+
+const UnknownErrorMetricsEventType metricsDetailEventType = "type.googleapis.com/bucketeer.event.client.UnknownErrorMetricsEvent"
+
+type UnknownErrorMetricsEvent struct {
+	APIID  APIID                  `json:"api_id,omitempty"`
+	Labels map[string]string      `json:"labels,omitempty"`
+	Type   metricsDetailEventType `json:"@type,omitempty"`
+}
+
+func NewUnknownErrorMetricsEvent(tag string, api APIID) *UnknownErrorMetricsEvent {
+	return &UnknownErrorMetricsEvent{
+		APIID:  api,
+		Labels: map[string]string{"tag": tag},
+		Type:   UnknownErrorMetricsEventType,
+	}
+}
