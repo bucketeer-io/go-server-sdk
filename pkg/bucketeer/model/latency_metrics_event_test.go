9fb78fe9af4fc9249e9cfd40e14c991c9c7f8d11
Naoto Ono <onoto1998@gmail.com>
2023-06-15 18:05:07 +0900

    chore: support LatencySecond field

--- pkg/bucketeer/model/latency_metrics_event_test.go
+++ pkg/bucketeer/model/latency_metrics_event_test.go
@@ -8,10 +8,9 @@ import (
 
 func TestNewLatencyMetricsEvent(t *testing.T) {
 	t.Parallel()
-	e := NewLatencyMetricsEvent(tag, value, GetEvaluation)
+	e := NewLatencyMetricsEvent(tag, 0.1, GetEvaluation)
 	assert.IsType(t, &LatencyMetricsEvent{}, e)
 	assert.Equal(t, tag, e.Labels["tag"])
-	assert.Equal(t, value, e.Duration.Value)
-	assert.Equal(t, DurationType, e.Duration.Type)
+	assert.Equal(t, 0.1, e.LatencySecond)
 	assert.Equal(t, LatencyMetricsEventType, e.Type)
 }
