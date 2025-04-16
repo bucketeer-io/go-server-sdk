4dce1e6e07c836280cf2cd61e70d8aaedf2ecbcd
Alessandro Yuichi Okimoto <yuichijpn@gmail.com>
2025-04-16 21:59:58 +0900

    chore: remove host option from example
    Signed-off-by: Alessandro Yuichi Okimoto <yuichijpn@gmail.com>

--- example/main.go
+++ example/main.go
@@ -16,15 +16,14 @@ import (
 )
 
 const (
-	timeout = 10 * time.Second
-
+	timeout   = 10 * time.Second
 	userIDKey = "user_id"
 )
 
 var (
 	bucketeerTag            = flag.String("bucketeer-tag", "", "Bucketeer tag")
 	bucketeerAPIKey         = flag.String("bucketeer-api-key", "", "Bucketeer api key")
-	bucketeerHost           = flag.String("bucketeer-host", "", "Bucketeer host name, e.g. api-dev.bucketeer.jp")
+	bucketeerAPIEndpoint    = flag.String("bucketeer-api-endpoint", "", "Bucketeer api endpoint, e.g. api.example.com")
 	bucketeerPort           = flag.Int("bucketeer-port", 443, "Bucketeer port number, e.g. 443")
 	bucketeerEnableDebugLog = flag.Bool("bucketeer-enable-debug-log", false, "Outpus sdk debug logs or not")
 	bucketerFeatureID       = flag.String("bucketeer-feature-id", "", "Target feature id")
@@ -41,7 +40,7 @@ func main() {
 		ctx,
 		bucketeer.WithTag(*bucketeerTag),
 		bucketeer.WithAPIKey(*bucketeerAPIKey),
-		bucketeer.WithHost(*bucketeerHost),
+		bucketeer.WithAPIEndpoint(*bucketeerAPIEndpoint),
 		bucketeer.WithEnableDebugLog(*bucketeerEnableDebugLog),
 	)
 	if err != nil {
